11b198301bbf117146959b8cef69a7a104b4a93b
Laurent L <lolo@truc.fr>
2018-07-20 12:25:59 +0200

    Ma version perso

---
+++ Repo4VS/Repo4VS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo4VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Git repo 4VS");
+            Console.WriteLine("Version 1");
+            Console.WriteLine("By me");
+            Console.Read();
+        }
+    }
+}
